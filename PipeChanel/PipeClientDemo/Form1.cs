792a5f98f14a1a41f18cb5f4cf42e8156fab1320
ashfrog <292340644@qq.com>
2017-07-05 17:29:11 +0800

    Add files via upload
    源码

---
+++ PipeChanel/PipeClientDemo/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PipeClientDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pipeclient = new PipeChanel.Pipechanel(2, "PipeClientDemo", "PipeServerDemo");
+            PipeChanel.Pipechanel.msgReceived+=new PipeChanel.PipeMsg.PipeMsgEventHandler(Pipechanel_msgReceived);
+        }
+        private void Pipechanel_msgReceived(object sender, PipeChanel.PipeMsg.PipeMsgEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate
+            {
+                this.listBox1.Items.Add(e.receivedMsg);
+            }));
+        }
+        PipeChanel.Pipechanel pipeclient;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pipeclient.Send(this.textBox1.Text);
+        }
+    }
+}
---
+++ PipeChanel/PipeServerDemo/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PipeServerDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pipeserver = new PipeChanel.Pipechanel(2, "PipeServerDemo","PipeClientDemo");
+            PipeChanel.Pipechanel.msgReceived += new PipeChanel.PipeMsg.PipeMsgEventHandler(Pipechanel_msgReceived);
+        }
+        private void Pipechanel_msgReceived(object sender, PipeChanel.PipeMsg.PipeMsgEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate
+            {
+                this.listBox1.Items.Add(e.receivedMsg);
+            }));
+        }
+        PipeChanel.Pipechanel pipeserver;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pipeserver.Send(this.textBox1.Text);
+        }
+    }
+}
